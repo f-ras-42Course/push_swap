f779cc23ac0ad1fade3dae522fbd11ceb99a3c94
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-04-27 01:45:40 +0200

    minor change

--- validations.c
+++ validations.c
@@ -6,11 +6,30 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 21:50:25 by fras          #+#    #+#                 */
-/*   Updated: 2023/04/26 23:33:18 by fras          ########   odam.nl         */
+/*   Updated: 2023/04/27 01:45:27 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *validate_input(int size, char *data[])
+int		check_correct_formatting(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 0;
+	while (i < argc)
+	{
+		if (!argv[i][j])
+			error_exit;
+		while (argv[i][j])
+		{
+			if (!ft_isdigit(argv[i][j]) || !argv[i][j] == ' ')
+				error_exit;
+		}
+	}
+}
+
+void	check_for_duplicates(t_list *data);
 {
 	int i;
 
@@ -19,4 +38,10 @@ char *validate_input(int size, char *data[])
 	{
 		data[i]
 	}
+}
+
+void	error_exit(void)
+{
+	write(2, "Error\n", 7);
+	exit(0);
 }
