565ce24a5af5d77ed7a430f7f7ce7195d176dd8f
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-04-27 03:19:33 +0200

    semicolon fix

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 21:53:14 by fras          #+#    #+#                 */
-/*   Updated: 2023/04/27 02:56:33 by fras          ########   odam.nl         */
+/*   Updated: 2023/04/27 03:18:44 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 int		validate_formatting(int argc, char *argv[]);
 int		collect_input(int argc, char *argv[]);
 void	validate_no_duplicates(t_list *data);
-void	error_exit(void)
+void	error_exit(void);
 t_list	lst_add_new_value(int value);
