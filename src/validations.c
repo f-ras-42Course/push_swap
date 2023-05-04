f7aa2d0a1213ba1a0a655ee4f94050028b31895a
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-05-04 03:40:11 +0200

    improved and fixed validations

--- src/validations.c
+++ src/validations.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 21:50:25 by fras          #+#    #+#                 */
-/*   Updated: 2023/05/01 21:40:11 by fras          ########   odam.nl         */
+/*   Updated: 2023/05/04 03:39:50 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,21 @@ void		validate_formatting(int argc, char *argv[])
 	int j;
 
 	i = 1;
-	while (i <= argc)
+	if (argc == 1)
+		error_exit();
+	while (i < argc)
 	{
 		j = 0;
 		if (!argv[i][j])
 			error_exit();
-		if (!ft_isdigit(argv[i][0]) || (argv[i][0] != '-'))
+		if (!ft_isdigit(argv[i][j]) || (argv[i][j] != '-'))
 			error_exit();
 		while (argv[i][j])
 		{
 			j++;
-			if (!ft_isdigit(argv[i][j]) || argv[i][j] != ' ' \
-				|| (argv[i][j] != '-') \
-				|| (j != 0 && (argv[i][j] == '-') && (argv[i][j - 1] != ' ')))
+			if (!ft_isdigit(argv[i][j]) \
+				|| (argv[i][j] == ' '  && !ft_isdigit(argv[i][j - 1])) \
+				|| (argv[i][j] == '-') && (argv[i][j - 1] != ' '))
 				error_exit();
 		}
 		i++;
