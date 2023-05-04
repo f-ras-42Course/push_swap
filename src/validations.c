63ea66adb06716d404c95e81e4fdc34e50ff6efd
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-05-04 05:41:33 +0200

    fixed validations

--- src/validations.c
+++ src/validations.c
@@ -6,37 +6,51 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 21:50:25 by fras          #+#    #+#                 */
-/*   Updated: 2023/05/04 03:39:50 by fras          ########   odam.nl         */
+/*   Updated: 2023/05/04 05:37:15 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void		validate_formatting(int argc, char *argv[])
+void	error_exit(void)
 {
-	int i;
-	int j;
+	write(2, "Error\n", 7);
+	exit(EXIT_FAILURE);
+}
+
+void	validate_formatting(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+	int	digit_count;
 
 	i = 1;
-	if (argc == 1)
-		error_exit();
 	while (i < argc)
 	{
 		j = 0;
-		if (!argv[i][j])
-			error_exit();
-		if (!ft_isdigit(argv[i][j]) || (argv[i][j] != '-'))
+		digit_count = 0;
+		if (!ft_isdigit(argv[i][0]) || (argv[i][0] != '-'))
 			error_exit();
 		while (argv[i][j])
 		{
-			j++;
-			if (!ft_isdigit(argv[i][j]) \
-				|| (argv[i][j] == ' '  && !ft_isdigit(argv[i][j - 1])) \
-				|| (argv[i][j] == '-') && (argv[i][j - 1] != ' '))
+			digit_count += ft_isdigit(argv[i][j]);
+			if (digit_count > 10)
 				error_exit();
+			j++;
+			if (!singlenum_format)
+				error_exit();
+			if (argv[i][j] == ' ')
+				digit_count = 0;
 		}
 		i++;
 	}
+}
+
+int	singlenum_format(char *ptr, int i)
+{
+	return (ft_isdigit(ptr[i]) \
+		|| ((ptr[i] == ' ') && ft_isdigit(ptr[i - 1])) \
+		|| ((ptr[i] == '-') && (ptr[i - 1] == ' ')));
 }
 
 // void	validate_no_duplicates(t_list *data)
@@ -49,9 +63,3 @@ void		validate_formatting(int argc, char *argv[])
 // 		data[i]
 // 	}
 // }
-
-void	error_exit(void)
-{
-	write(2, "Error\n", 7);
-	exit(EXIT_FAILURE);
-}
