e1040860ec07423829c33f9b378d650a830c84b4
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-05-17 03:56:02 +0200

    improved layout and removed unnecesarry comments (commit: 6e72dd47)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 21:20:13 by fras          #+#    #+#                 */
-/*   Updated: 2023/05/17 03:27:24 by ferryras      ########   odam.nl         */
+/*   Updated: 2023/05/17 03:54:52 by ferryras      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,14 @@
 
 int	main(int argc, char *argv[])
 {
-	// t_data **stackA;
-	// t_data **stackB;
 	t_data *data;
-	t_data *ptr;
-	int i;
 
-	i = 0;
 	if (argc == 1)
 		error_exit();
 	validate_formatting(argv);
 	data = collect_data(argv);
 	validate_no_duplicate_num(data);
 	add_normalized_values(data);
-	// ptr = data;
-	// printf("[%d] = %d, ", i++, ptr->input_value);
-	// ptr = ptr->next;
-	// while (ptr != data)
-	// {
-	// 	printf("[%d] = %d, ", i++, ptr->input_value);
-	// 	ptr = ptr->next;
-	// }
-	// printf("[%d] = %d, ", i++, ptr->input_value);
-	// Output:
-	// ./push_swap 4463 24 544 344
-	//[0] = 4463, [1] = 24, [2] = 544, [3] = 344, [4] = 4463, 
+	push_swap(data);
 	return (0);
 }
