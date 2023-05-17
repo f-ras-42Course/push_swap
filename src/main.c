229a0f2ad2d41aadd80006ce0953bb7ca9844e3c
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-05-17 18:53:00 +0200

    removed tester from main, put it in another C file

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 21:20:13 by fras          #+#    #+#                 */
-/*   Updated: 2023/05/17 10:13:30 by fras          ########   odam.nl         */
+/*   Updated: 2023/05/17 18:51:00 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,42 +26,6 @@ int	main(int argc, char *argv[])
 	insert_normalized_values(data, circular_list_size(data));
 	push_swap(data);
 	
-	test(data); // - temp for development
+	// test(data); // - temp for development
 	return (0);
-}
-
-
-// temporary tester:
-void test (t_data *data)
-{
-	t_data *ptr;
-	int i;
-
-	printf("\nINPUT VALUES:\n");
- 	i = 0;
-	ptr = data;
- 	printf("[HEAD %d] = %d, ", i++, ptr->input_value);
- 	ptr = ptr->next;
- 	while (ptr != data)
- 	{
- 		printf("[%d] = %d, ", i++, ptr->input_value);
- 		ptr = ptr->next;
- 	}
- 	printf("\n[HEAD check] = %d\n", ptr->input_value);
-	
-	printf("\nNORMALIZED VALUES:\n");
-	i = 0;
-	ptr = data;
- 	printf("[HEAD %d] = %d, ", i++, ptr->normalized_value);
- 	ptr = ptr->next;
- 	while (ptr != data)
- 	{
- 		printf("[%d] = %d, ", i++, ptr->normalized_value);
- 		ptr = ptr->next;
- 	}
-	printf("\n\n");
- 	// Output:
- 	// ./push_swap 4463 24 544 344
- 	// [0] = 4463, [1] = 24, [2] = 544, [3] = 344, [4] = 4463, 
-	// push_swap(data);
 }
---
+++ src/test.c
@@ -0,0 +1,68 @@
+// temporary tester:
+
+#include "push_swap.h"
+
+void test (t_data *data)
+{
+	t_data *ptr;
+	int i;
+
+	printf("\n -- NEXT -- \n");
+
+	printf("\nINPUT VALUES:\n");
+ 	i = 0;
+	ptr = data;
+ 	printf("[HEAD %d] = %d, ", i++, ptr->input_value);
+ 	ptr = ptr->next;
+ 	while (ptr != data)
+ 	{
+ 		printf("[%d] = %d, ", i++, ptr->input_value);
+ 		ptr = ptr->next;
+ 	}
+ 	printf("\n[HEAD check] = %d\n", ptr->input_value);
+	
+	// printf("\nNORMALIZED VALUES:\n");
+	// i = 0;
+	// ptr = data;
+ 	// printf("[HEAD %d] = %d, ", i++, ptr->normalized_value);
+ 	// ptr = ptr->next;
+ 	// while (ptr != data)
+ 	// {
+ 	// 	printf("[%d] = %d, ", i++, ptr->normalized_value);
+ 	// 	ptr = ptr->next;
+ 	// }
+	// printf("\n[HEAD check] = %d\n", ptr->normalized_value);
+
+	printf("\n\n");
+
+	printf("\n -- PREV -- \n");
+	i = 0;
+	ptr = data;
+ 	printf("[HEAD %d] = %d, ", i++, ptr->input_value);
+ 	ptr = ptr->prev;
+ 	while (ptr != data)
+ 	{
+ 		printf("[%d] = %d, ", i++, ptr->input_value);
+ 		ptr = ptr->prev;
+ 	}
+ 	printf("\n[HEAD check] = %d\n", ptr->input_value);
+	
+	// printf("\nNORMALIZED VALUES:\n");
+	// i = 0;
+	// ptr = data;
+ 	// printf("[HEAD %d] = %d, ", i++, ptr->normalized_value);
+ 	// ptr = ptr->prev;
+ 	// while (ptr != data)
+ 	// {
+ 	// 	printf("[%d] = %d, ", i++, ptr->normalized_value);
+ 	// 	ptr = ptr->prev;
+ 	// }
+	// printf("\n[HEAD check] = %d\n", ptr->normalized_value);
+
+	printf("\n\n");
+
+ 	// Output:
+ 	// ./push_swap 4463 24 544 344
+ 	// [0] = 4463, [1] = 24, [2] = 544, [3] = 344, [4] = 4463, 
+	// push_swap(data);
+}
