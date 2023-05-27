0cd5fa52e814ddf13ca7ee2607bf6d52c6525ec5
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-05-27 17:39:48 +0200

    erased, function needs to be different

--- src/operationsRotate.c
+++ src/operationsRotate.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 10:31:59 by fras          #+#    #+#                 */
-/*   Updated: 2023/05/27 16:55:03 by fras          ########   odam.nl         */
+/*   Updated: 2023/05/27 17:39:00 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,17 @@
 
 void	rotate_a (t_data **stackA)
 {
-	t_data	*tail_a;
+	t_data	*head_a;
 
-	tail_a = (*stackA)->prev;
-	(*stackA)->prev = tail_a->prev;
-	tail_a->next = (*stackA)->next;
-	tail_a->prev = *stackA;
-	(*stackA)->next = tail_a;
-	*stackA = tail_a;
+	head_a = *stackA;
 	ft_printf("ra\n");
 }
 
 void	rotate_b (t_data **stackB)
 {
-	t_data	*tail_b;
+	t_data	*head_b;
 
-	tail_b = (*stackB)->prev;
-	(*stackB)->prev = tail_b->prev;
-	(*stackB)->next = tail_b;
-	tail_b->next = (*stackB)->next;
-	tail_b->prev = *stackB;
-	*stackB = tail_b;
+	head_b = *stackB;
 	ft_printf("rb\n");
 }
 
