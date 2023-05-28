0a77e5a686dfb0836114bffa538c312817fc9e09
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-05-28 02:16:27 +0200

    undo removal function

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 21:53:14 by fras          #+#    #+#                 */
-/*   Updated: 2023/05/28 02:05:05 by fras          ########   odam.nl         */
+/*   Updated: 2023/05/28 02:12:48 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ t_data	*collect_data(char *argv[]);
 t_data	*new_data_list(int input);
 t_data	*add_data_node(t_data *prev, int input, t_data *next);
 t_data	*split_nums_from_strings(t_data *ptr, char *str, t_data *head);
+ size_t	circular_list_size(t_data *head);
 void	insert_normalized_values(t_data *head, size_t size);
 void	push_swap(t_data *start_input);
 void	print_ops(t_cmd operation);
---
+++ src/utils.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+ /*                                                                            */
+ /*                                                        ::::::::            */
+ /*   utils.c                                            :+:    :+:            */
+ /*                                                     +:+                    */
+ /*   By: fras <fras@student.codam.nl>                 +#+                     */
+ /*                                                   +#+                      */
+ /*   Created: 2023/05/17 04:07:43 by fras          #+#    #+#                 */
+ /*   Updated: 2023/05/17 10:14:11 by fras          ########   odam.nl         */
+ /*                                                                            */
+ /* ************************************************************************** */
+
+ #include "push_swap.h"
+
+ size_t	circular_list_size(t_data *head)
+ {
+ 	size_t	size;
+ 	t_data	*ptr;
+
+ 	size = 0;
+ 	ptr = head->next;
+ 	while (ptr != head)
+ 	{
+ 		ptr = ptr->next;
+ 		size++;
+ 	}
+ 	return (++size);
+ }
+ 
