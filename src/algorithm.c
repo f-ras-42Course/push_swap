86b2996ce5491a54e86490ed22d7d90a9a97fc6b
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-06 15:54:17 +0000

    Splitted helper functions into other file

--- src/algorithm.c
+++ src/algorithm.c
@@ -43,35 +43,3 @@ void	redix_sort(t_data **stackA, t_data **stackB)
 	}
 	printf("\n\n\n\n\nHIGHEST BIT-LENGTH = %d\n\n\n\n\n\n", bit_length);
 }
-
-void	push_all_to_stackA(t_data **stackA, t_data **stackB)
-{
-	t_cmd command;
-
-	command = print_ops(push_a(stackA, stackB));
-	while(command != none)
-		command = print_ops(push_a(stackA, stackB));
-}
-
-int	get_highest_bit_length(t_data *stackA)
-{
-	t_data	*ptr;
-	int		highest_value;
-	int		bit_length;
-
-	highest_value = stackA->normalized_value;
-	ptr = stackA->next;
-	while (ptr != stackA)
-	{
-		if (highest_value <= ptr->normalized_value)
-			highest_value = ptr->normalized_value;
-		ptr = ptr->next;
-	}
-	bit_length = 0;
-	while (highest_value)
-	{
-		highest_value /= 2;
-		bit_length++;
-	}
-	return (bit_length);
-}
