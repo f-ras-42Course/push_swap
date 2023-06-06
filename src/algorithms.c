c5ca1eddbc03ae5b9f983d34c6ca2e77ab515a23
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-06 20:03:40 +0200

    refactor function - fewer lines, better readable

--- src/algorithms.c
+++ src/algorithms.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/29 18:26:53 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/06 16:40:19 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/06 20:01:52 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,31 +44,27 @@ void	small_sort(t_data **stackA, size_t data_nodes)
 void	redix_sort(t_data **stackA, t_data **stackB)
 {
 	int		bit_length;
+	int		bit_check;
 	size_t	stack_size;
 	size_t	i;
 	
 	i = 0;
+	bit_check = 1;
 	bit_length = get_highest_bit_length(*stackA);
 	stack_size = circular_list_size(*stackA);
 	while (bit_length)
 	{
-		if (!((*stackA)->normalized_value &1))
-		{
-			(*stackA)->normalized_value = (*stackA)->normalized_value >> 1;
+		if (((*stackA)->normalized_value &bit_check) == 0)
 			print_ops(push_b(stackA, stackB));
-		}
 		else
-		{
-			(*stackA)->normalized_value = (*stackA)->normalized_value >> 1;
 			print_ops(rotate_a(stackA));
-		}
 		i++;
 		if (i == stack_size)
 		{
+			bit_check *= 2;
 			push_all_to_stackA(stackA, stackB);
 			i = 0;
 			bit_length--;
 		}
 	}
-	printf("\n\n\n\n\nHIGHEST BIT-LENGTH = %d\n\n\n\n\n\n", bit_length);
 }
