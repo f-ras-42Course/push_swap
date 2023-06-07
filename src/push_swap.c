335edc52c856844ff50fc62c18e2e04cbb9dbf1c
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-07 23:16:32 +0200

    removed test calls

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 10:10:25 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/07 23:14:47 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/07 23:15:37 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,10 @@ void	push_swap(t_data *start_input, size_t data_size)
 
 	stack_a = start_input;
 	stack_b = NULL;
-	test(stack_a);
-	test(stack_b);
 	if (data_size <= 3)
 		small_sort(&stack_a, data_size, 0);
 	else if (data_size <= 73)
 		ferry_sort (&stack_a, &stack_b, data_size);
 	else
 		redix_sort(&stack_a, &stack_b, data_size);
-	test(stack_a);
-	test(stack_b);
-// 	if (list_size <= 3)
-// 		xx1sort;
-// 	else if (list_size <= 5)
-// 		xx2Sort;
-// 	else
-// 		RadixSort;
-// }
 }
