e08562f485710f4a97533f63194021dcc7b860c5
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-07 23:55:49 +0200

    cleaned up code

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/26 21:20:13 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/07 23:17:15 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/07 23:55:37 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,5 @@ int	main(int argc, char *argv[])
 	data_size = circular_list_size(data);
 	insert_normalized_values(data, data_size);
 	push_swap(data, data_size);
-	
-	// test(data); // - temp for development
 	return (0);
 }
