c2f2c4be0fd8f746efacb6953a91ab69c10c6949
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-07 15:04:15 +0200

    seperated sorting functions

---
+++ src/algorithmsFerry.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   algorithmsFerry.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fras <fras@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/07 15:01:02 by fras          #+#    #+#                 */
+/*   Updated: 2023/06/07 15:04:02 by fras          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ferry_sort(t_data **stackA, t_data **stackB, size_t data_size)
+{
+	int		i;
+	int		check;
+	t_data	*ptr;
+
+	i = 0;
+	ptr = *stackA;
+	if (data_size == 4)
+	{
+		while(ptr->normalized_value)
+	}
+}
