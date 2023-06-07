6d0a2d5141e8ab95dc3ebf6939de689a20deff09
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-07 16:26:58 +0200

    refactor

--- src/algorithmsFerry.c
+++ src/algorithmsFerry.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/07 15:01:02 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/07 16:22:54 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/07 16:26:42 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,19 @@ void	ferry_sort(t_data **stackA, t_data **stackB, size_t data_size)
 
 void	ferry_sort_sz4(t_data **stackA, t_data **stackB)
 {
-	int i;
+	int lowest_not_on_top;
 
-	i = 0;
+	lowest_not_on_top = 0;
 	if (locate_low_normal_or_rev(stackA, 0))
-		while ((*stackA)->normalized_value != 0 && ++i)
+		while ((*stackA)->normalized_value != 0 && ++lowest_not_on_top)
 			print_ops(rotate_a(stackA));
 	else
-		while ((*stackA)->normalized_value != 0 && ++i)
+		while ((*stackA)->normalized_value != 0 && ++lowest_not_on_top)
 			print_ops(rev_rotate_a(stackA));
-	if (i)
+	if (lowest_not_on_top)
 		print_ops(push_b(stackA, stackB));
 	small_sort(stackA, 3, 1);
-	if (i)
+	if (lowest_not_on_top)
 		print_ops(push_a(stackA, stackB));
 }
 
