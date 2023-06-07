cb110d5c02286e3c0d04640e39de7a336a8096f9
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-07 14:51:02 +0200

    changed file to pre-write intentions

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 10:10:25 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/07 08:09:17 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/07 14:50:30 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,12 @@ void	push_swap(t_data *start_input)
 	stack_b = NULL;
 	test(stack_a);
 	test(stack_b);
-	redix_sort(&stack_a, &stack_b, data_size);
+	if (data_size <= 3)
+		small_sort(stack_a, data_size, 0);
+	else if (data_size <= 6)
+		ferry_sort (stack_a, stack_b, data_size);
+	else
+		redix_sort(&stack_a, &stack_b, data_size);
 	test(stack_a);
 	test(stack_b);
 // 	if (list_size <= 3)
