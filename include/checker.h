b30d710b7a5c50ff5a5a6c1923f81698c1562872
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-08 13:48:34 +0200

    checker prototyping update

--- include/checker.h
+++ include/checker.h
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/08 00:46:16 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/08 01:21:56 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/08 13:45:51 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "push_swap.h"
 
-void	checker(data, data_size);
+void	checker(t_data *start_input, size_t data_size);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
--- src/checker.c
+++ src/checker.c
@@ -6,11 +6,13 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/08 01:21:18 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/08 01:21:41 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/08 13:47:53 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	checker(data, data_size)
+#include "checker.h"
+
+void	checker(t_data *start_input, size_t data_size)
 {
 	
 }
