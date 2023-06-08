3a7d315b5e6fdef13aeb12717bae44876cc069f6
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-08 17:12:34 +0200

    changed to lowercase

--- src/checker/get_next_line/PS_get_next_line_errors.c
+++ src/checker/get_next_line/PS_get_next_line_errors.c
@@ -6,13 +6,13 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/08 15:52:15 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/08 16:08:52 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/08 17:12:08 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-void	EXIT_MALLOC_FAILURE(void)
+void	malloc_failure_exit(void)
 {
 	write(STDERR_FILENO, "PROGRAM ERROR: malloc failed.\n", 31);
 	exit(EXIT_FAILURE);
