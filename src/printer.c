b23d66e86a02ababa1fbc6d78f1889210ecc8d5a
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-08 13:47:41 +0200

    refactor

--- src/printer.c
+++ src/printer.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/27 19:05:43 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/07 23:37:20 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/08 13:42:58 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@
 t_cmd	print_ops(t_cmd operation)
 {
 	if (operation == sa)
-		write(1, "sa\n", 3);
+		write(STDOUT_FILENO, "sa\n", 3);
 	if (operation == sb)
-		write(1, "sb\n", 3);
+		write(STDOUT_FILENO, "sb\n", 3);
 	if (operation == ss)
-		write(1, "ss\n", 3);
+		write(STDOUT_FILENO, "ss\n", 3);
 	if (operation == pa)
-		write(1, "pa\n", 3);
+		write(STDOUT_FILENO, "pa\n", 3);
 	if (operation == pb)
-		write(1, "pb\n", 3);
+		write(STDOUT_FILENO, "pb\n", 3);
 	if (operation == ra)
-		write(1, "ra\n", 3);
+		write(STDOUT_FILENO, "ra\n", 3);
 	if (operation == rb)
-		write(1, "rb\n", 3);
+		write(STDOUT_FILENO, "rb\n", 3);
 	if (operation == rr)
-		write(1, "rr\n", 3);
+		write(STDOUT_FILENO, "rr\n", 3);
 	if (operation == rra)
-		write(1, "rra\n", 4);
+		write(STDOUT_FILENO, "rra\n", 4);
 	if (operation == rrb)
-		write(1, "rrb\n", 4);
+		write(STDOUT_FILENO, "rrb\n", 4);
 	if (operation == rrr)
-		write(1, "rrr\n", 4);
+		write(STDOUT_FILENO, "rrr\n", 4);
 	return(operation);
 }
