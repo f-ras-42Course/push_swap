f7fb967d27fc9ea2cab2a876c6484196b1bea05e
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-06-10 13:19:20 +0200

    refactor

--- include/checker.h
+++ include/checker.h
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/08 00:46:16 by fras          #+#    #+#                 */
-/*   Updated: 2023/06/10 13:14:25 by fras          ########   odam.nl         */
+/*   Updated: 2023/06/10 13:19:11 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 # include "push_swap.h"
 
 void	checker(t_data *data_input);
-char	*get_next_line_stdin(void);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	sort_stack_from_stdin(t_data **stackA, t_data **stackB);
+char	*get_next_line_stdin(void);
 t_cmd	get_operations(char *str);
 void	make_operations(t_cmd command, t_data **stackA, t_data **stackB);
 void	print_result(t_data **stackA, t_data **stackB);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
